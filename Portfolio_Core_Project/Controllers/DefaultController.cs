eb603ab5425a7311141018b7fb24f253ef825302
Özlem Karakoç <ozlmkrkoc@gmail.com>
2024-09-17 08:22:58 +0300

    ...

---
+++ Portfolio_Core_Project/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Core_Project.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+    }
+}
