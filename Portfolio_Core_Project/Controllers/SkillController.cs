4f0b1ba3c22475b54cfef76ceb205f2d3d36df4a
Özlem Karakoç <ozlmkrkoc@gmail.com>
2024-09-22 18:57:19 +0300

    ...

--- Portfolio_Core_Project/Controllers/SkillController.cs
+++ Portfolio_Core_Project/Controllers/SkillController.cs
@@ -1,12 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Portfolio_Core_Project.Controllers
 {
     public class SkillController : Controller
     {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
         public IActionResult Index()
         {
+            ViewBag.v1 = "Yetenek Listesi";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Listesi";
+            var values = skillManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            ViewBag.v1 = "Yetenek Ekleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Ekleme";
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddSkill(Skill skill)
+        {
+            skillManager.TAdd(skill);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteeSkill(int id)
+        {
+            var values = skillManager.TGetByID(id);
+            skillManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditSkill(int id)
+        {
+            ViewBag.v1 = "Yetenek Güncelleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Güncelleme";
+            var values = skillManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditSkill(Skill skill)
+        {
+            skillManager.TUpdate(skill);
+            return RedirectToAction("Index");
         }
     }
 }
