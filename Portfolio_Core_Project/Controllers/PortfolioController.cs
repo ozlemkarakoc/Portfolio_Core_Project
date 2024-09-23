d3dcd4f835021e5b6c95c813e4114f3a99a215e6
Özlem Karakoç <ozlmkrkoc@gmail.com>
2024-09-24 00:29:58 +0300

    ...

--- Portfolio_Core_Project/Controllers/PortfolioController.cs
+++ Portfolio_Core_Project/Controllers/PortfolioController.cs
@@ -56,5 +56,33 @@ namespace Portfolio_Core_Project.Controllers
             portfolioManager.TDelete(values);
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        public IActionResult EditPortfolio(int id)
+        {
+            ViewBag.v1 = "Proje Güncelleme";
+            ViewBag.v2 = "Projeler";
+            ViewBag.v3 = "Proje Güncelleme";
+            var values = portfolioManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditPortfolio(Portfolio portfolio)
+        {
+            PortfolioValidator validations = new PortfolioValidator();
+            ValidationResult results = validations.Validate(portfolio);
+            if (results.IsValid)
+            {
+                portfolioManager.TUpdate(portfolio);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach(var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
     }
 }
