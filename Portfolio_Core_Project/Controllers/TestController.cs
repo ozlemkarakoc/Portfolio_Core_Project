8c9694a941f6d3ffc08e8238b29cb5e432b4e40d
Özlem Karakoç <ozlmkrkoc@gmail.com>
2024-09-24 22:14:39 +0300

    ...

---
+++ Portfolio_Core_Project/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Core_Project.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
