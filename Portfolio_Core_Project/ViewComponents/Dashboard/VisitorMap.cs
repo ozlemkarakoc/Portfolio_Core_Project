becd95bb0004ee253243f853ca62d258e1181bab
Özlem Karakoç <ozlmkrkoc@gmail.com>
2024-10-02 14:03:14 +0300

    ...

---
+++ Portfolio_Core_Project/ViewComponents/Dashboard/VisitorMap.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Core_Project.ViewComponents.Dashboard
+{
+    public class VisitorMap : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
