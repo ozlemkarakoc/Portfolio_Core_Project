607838bd317c33764bb4d0f8b0250785663d5f44
Özlem Karakoç <ozlmkrkoc@gmail.com>
2024-10-04 13:46:35 +0300

    ...

---
+++ Portfolio_Core_Project/Areas/Writer/Controllers/RegisterController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio_Core_Project.Areas.Writer.Models;
+
+namespace Portfolio_Core_Project.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(UserRegisterViewModel p)
+        {
+            if(ModelState.IsValid)
+            {
+                
+            }
+            return View();
+        }
+    }
+}
---
+++ Portfolio_Core_Project/Areas/Writer/Models/UserRegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio_Core_Project.Areas.Writer.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage = "Lütfen Kullanıcı Adını Girin!")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Kullanıcı Şifreyi Girin!")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Kullanıcı Şifreti Tekrar Girin!")]
+        [Compare("Password", ErrorMessage = "Şifreler Uyumlu Değil!")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Mail Adresini Girin")]
+        public string Mail { get; set; }
+    }
+}
