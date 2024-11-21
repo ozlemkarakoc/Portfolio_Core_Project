9ab66ca07d879e439b3cd9e8de4d444504e32248
Özlem Karakoç <ozlmkrkoc@gmail.com>
2024-11-21 14:15:44 +0300

    ...

---
+++ Portfolio_Core_Project/Controllers/AdminMessageController.cs
@@ -0,0 +1,64 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace Portfolio_Core_Project.Controllers
+{
+    public class AdminMessageController : Controller
+    {
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+        public IActionResult ReceiverMessageList()
+        {
+            string p;
+            p = "ozlmkrkoc@gmail.com";
+            var values = writerMessageManager.GetListReceiverMessage(p);
+            return View(values);
+        }
+        public IActionResult SenderMessageList()
+        {
+            string p;
+            p = "ozlmkrkoc@gmail.com";
+            var values = writerMessageManager.GetListSenderMessage(p);
+            return View(values);
+        }
+        public IActionResult AdminMessageDetails(int id)
+        {
+            var values = writerMessageManager.TGetByID(id);
+            return View(values);
+        }
+        public IActionResult AdminMessageDelete(int id)
+        {
+            var values = writerMessageManager.TGetByID(id);
+            writerMessageManager.TDelete(values);
+
+            if(values.SenderName!=null)
+            {
+                return RedirectToAction("ReceiverMessageList");
+
+            }
+            return RedirectToAction("SenderMessageList");
+        }
+
+        [HttpGet]
+        public IActionResult AdminMessageSend()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AdminMessageSend(WriterMessage p)
+        {
+            p.Sender = "ozlmkrkoc@gmail.com";
+            p.SenderName = "Özlem Karakoç";
+            p.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            Context c = new Context();
+            var usernamesurname = c.Users.Where(x => x.Email == p.Receiver).Select(y => y.Name + " " + y.Surname).FirstOrDefault();
+            p.ReceiverName = usernamesurname;
+            writerMessageManager.TAdd(p);
+            return RedirectToAction("SenderMessageList");
+        }
+    }
+}
