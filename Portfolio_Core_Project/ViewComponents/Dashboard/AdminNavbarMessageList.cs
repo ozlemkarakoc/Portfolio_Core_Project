bdcda66d6b15f88058fde990a186524827e62586
Özlem Karakoç <ozlmkrkoc@gmail.com>
2024-11-26 22:59:36 +0300

    ...

---
+++ Portfolio_Core_Project/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Core_Project.ViewComponents.Dashboard
+{
+    public class AdminNavbarMessageList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Portfolio_Core_Project/ViewComponents/SocialMedia/SocialMediaList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Core_Project.ViewComponents.SocialMedia
+{
+    public class SocialMediaList : ViewComponent
+    {
+        SocialMediaManager socialMedia = new SocialMediaManager(new EfSocialMediaDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = socialMedia.TGetList();
+            return View(values);
+        }
+    }
+}
