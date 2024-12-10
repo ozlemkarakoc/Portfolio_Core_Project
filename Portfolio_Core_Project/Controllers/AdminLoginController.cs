d9d64c38050b8dbf804b05596ae88fc9ba02ea20
Özlem Karakoç <ozlmkrkoc@gmail.com>
2024-12-10 14:03:08 +0300

    ...

---
+++ Portfolio_Core_Project/Controllers/AdminLoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Core_Project.Controllers
+{
+    public class AdminLoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Portfolio_Core_Project/ViewComponents/Dashboard/AdminNavbarMessageList.cs
+++ Portfolio_Core_Project/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Portfolio_Core_Project.ViewComponents.Dashboard
 {
     public class AdminNavbarMessageList : ViewComponent
     {
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            //Context c = new Context();
+            //foreach(var item in values)
+            //{
+            //    var img = c.Users.Where(x => x.Email == item.Sender).Select(x => x.ImageUrl).FirstOrDefault();
+            //    item.ImageUrl = img;
+            //}
+            string p = "ozlmkrkoc@gmail.com";
+            var values = writerMessageManager.GetListReceiverMessage(p).OrderByDescending(x => x.WriterMessageID).Take(3).ToList();
+            return View(values);
         }
 
     }
