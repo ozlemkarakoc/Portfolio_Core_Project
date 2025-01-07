8c43c4855d42a37b3d31581f258d017d3b9638ad
Özlem Karakoç <ozlmkrkoc@gmail.com>
2025-01-07 13:28:52 +0300

    ....

--- Portfolio_Core_Project/Controllers/TestimonialController.cs
+++ Portfolio_Core_Project/Controllers/TestimonialController.cs
@@ -13,6 +13,18 @@ namespace Portfolio_Core_Project.Controllers
             var values = testimonialManager.TGetList();
             return View(values);
         }
+        [HttpGet]
+        public IActionResult AddTestimonial()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddTestimonial(Testimonial p)
+        {
+            testimonialManager.TAdd(p);
+            return RedirectToAction("Index");
+        }
         public IActionResult DeleteTestimonial(int id)
         {
             var values = testimonialManager.TGetByID(id);
